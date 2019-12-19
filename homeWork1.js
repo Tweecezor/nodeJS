cb6e362452b46346f3c7e7093aae90f713df1b43
Tweecezor <thenamelessmanhd@gmail.com>
2019-12-19 16:36:32 +0300

    async 1 dz

--- homeWork1.js
+++ homeWork1.js
@@ -5,46 +5,56 @@ const minimist = require("minimist");
 
 var { srcFolder, newFolder, deleteStatus } = minimist(process.argv.slice(2));
 
+var count = 0;
+
 const _path = path.join(__dirname, srcFolder);
 
+function mainFunction(cb) {
+  readDir(_path);
+  cb();
+}
+
 const readDir = base => {
+  console.log(`count = ${count++}`);
   const folders = fs.readdirSync(base);
   folders.forEach(item => {
     if (item !== ".DS_Store") {
-      createNewFolder(base, item);
+      console.log(item);
+      let localBase = path.join(base, item);
+
+      let newMainFolder = path.join(__dirname, newFolder);
+
+      state = fs.statSync(localBase);
+      if (state.isDirectory()) {
+        if (!fs.existsSync(newMainFolder)) {
+          fs.mkdirSync(newMainFolder);
+        }
+        readDir(localBase);
+      } else {
+        const newDir = path.join(newMainFolder, item.slice(0, 1));
+
+        if (!fs.existsSync(newDir.toUpperCase())) {
+          fs.mkdirSync(newDir.toUpperCase());
+        }
+        console.log("safdsgre");
+        fs.link(localBase, path.join(newDir, item), () => {
+          if (fs.existsSync(path.join(__dirname, srcFolder))) {
+            console.log("before delete");
+            if (deleteStatus == "true") {
+              console.log("deleted");
+              del([path.join(__dirname, srcFolder)]);
+            }
+          }
+        });
+      }
     }
   });
 };
 
 function createNewFolder(base, item) {
-  let localBase = path.join(base, item);
-
-  let newMainFolder = path.join(__dirname, newFolder);
-
-  let state = fs.statSync(localBase);
-
-  if (state.isDirectory()) {
-    if (!fs.existsSync(newMainFolder)) {
-      fs.mkdirSync(newMainFolder);
-    }
-    readDir(localBase);
-  } else {
-    const newDir = path.join(newMainFolder, item.slice(0, 1));
-
-    if (!fs.existsSync(newDir.toUpperCase())) {
-      fs.mkdirSync(newDir.toUpperCase());
-    }
-    fs.linkSync(localBase, path.join(newDir, item), err => {
-      if (err) {
-        console.error(err.message);
-        return;
-      }
-    });
-  }
+  console.log("ff");
 }
 
-readDir(_path, 0);
-
-if (deleteStatus == "true") {
-  del([path.join(__dirname, srcFolder)]);
-}
+mainFunction(() => {
+  console.log("CBCBCBCBCBCB");
+});
